45af0363a7c7fa871410ce1e7053a738de130030
Mohidul Haque Mridul <mridulhaque@Mohiduls-MacBook-Air.local>
2023-04-12 01:00:04 +0600

    fixed full profile patch issue

--- users/serializers/profile.py
+++ users/serializers/profile.py
@@ -72,11 +72,11 @@ class FullProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
     email_address = serializers.CharField(source='user.email_address', read_only=True)
 
-    social_media_links = SocialMediaLinkSerializer(many=True)
-    present_address = PresentAddressSerializer()
-    skills = SkillSerializer(many=True)
-    academic_histories = AcademicHistorySerializer(many=True)
-    work_experiences = WorkExperienceSerializer(many=True)
+    social_media_links = SocialMediaLinkSerializer(many=True, required = False)
+    present_address = PresentAddressSerializer(required = False)
+    skills = SkillSerializer(many=True, required = False)
+    academic_histories = AcademicHistorySerializer(many=True, required = False)
+    work_experiences = WorkExperienceSerializer(many=True, required = False)
 
     class Meta:
         model = Profile
@@ -88,51 +88,11 @@ class FullProfileSerializer(serializers.ModelSerializer):
         if instance.user != user:
             raise serializers.ValidationError('Cannot update profile of another user')
         
-        social_media_links_data = validated_data.pop('social_media_links', [])
-        present_address_data = validated_data.pop('present_address', {})
-        skills_data = validated_data.pop('skills', [])
-        academic_histories_data = validated_data.pop('academic_histories', [])
-        work_experiences_data = validated_data.pop('work_experiences', [])
-        
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
-        
-        instance.social_media_links.all().delete()
-        for link_data in social_media_links_data:
-            SocialMediaLink.objects.create(profile=instance, **link_data)
-        
-        try:
-            instance.present_address.delete()
-        except:
-            pass
-        PresentAddress.objects.create(profile=instance, **present_address_data)
-        
-        instance.skills.all().delete()
-        for skill_data in skills_data:
-            Skill.objects.create(profile=instance, **skill_data)
-        
-        instance.academic_histories.all().delete()
-        for academic_history_data in academic_histories_data:
-            AcademicHistory.objects.create(profile=instance, **academic_history_data)
-        
-        instance.work_experiences.all().delete()
-        for work_experience_data in work_experiences_data:
-            WorkExperience.objects.create(profile=instance, **work_experience_data)
-        
-        instance.save()
-        
-        return instance
-    
-    def partial_update(self, instance, validated_data):
-        user = self.context['request'].user
-        if instance.user != user:
-            raise serializers.ValidationError('Cannot update profile of another user')
-        
-        social_media_links_data = validated_data.pop('social_media_links', [])
-        present_address_data = validated_data.pop('present_address', {})
-        skills_data = validated_data.pop('skills', [])
-        academic_histories_data = validated_data.pop('academic_histories', [])
-        work_experiences_data = validated_data.pop('work_experiences', [])
+        social_media_links_data = validated_data.pop('social_media_links', None)
+        present_address_data = validated_data.pop('present_address', None)
+        skills_data = validated_data.pop('skills', None)
+        academic_histories_data = validated_data.pop('academic_histories', None)
+        work_experiences_data = validated_data.pop('work_experiences', None)
         
         for key, value in validated_data.items():
             setattr(instance, key, value)
@@ -159,7 +119,52 @@ class FullProfileSerializer(serializers.ModelSerializer):
             for academic_history_data in academic_histories_data:
                 AcademicHistory.objects.create(profile=instance, **academic_history_data)
         
-        if work_experience_data:
+        if work_experiences_data:
+            instance.work_experiences.all().delete()
+            for work_experience_data in work_experiences_data:
+                WorkExperience.objects.create(profile=instance, **work_experience_data)
+        
+        instance.save()
+        
+        return instance
+    
+    def partial_update(self, instance, validated_data):
+        user = self.context['request'].user
+        if instance.user != user:
+            raise serializers.ValidationError('Cannot update profile of another user')
+        
+        social_media_links_data = validated_data.pop('social_media_links', None)
+        present_address_data = validated_data.pop('present_address', None)
+        skills_data = validated_data.pop('skills', None)
+        academic_histories_data = validated_data.pop('academic_histories', None)
+        work_experiences_data = validated_data.pop('work_experiences', None)
+        
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        
+        if social_media_links_data:
+            instance.social_media_links.all().delete()
+            for link_data in social_media_links_data:
+                SocialMediaLink.objects.create(profile=instance, **link_data)
+
+        if present_address_data:
+            try:
+                instance.present_address.delete()
+            except:
+                pass
+            PresentAddress.objects.create(profile=instance, **present_address_data)
+        
+        if skills_data:
+            instance.skills.all().delete()
+            for skill_data in skills_data:
+                Skill.objects.create(profile=instance, **skill_data)
+        
+        if academic_histories_data:
+            instance.academic_histories.all().delete()
+            for academic_history_data in academic_histories_data:
+                AcademicHistory.objects.create(profile=instance, **academic_history_data)
+        
+        if work_experiences_data:
             instance.work_experiences.all().delete()
             for work_experience_data in work_experiences_data:
                 WorkExperience.objects.create(profile=instance, **work_experience_data)
--- users/views/fullprofile.py
+++ users/views/fullprofile.py
@@ -31,17 +31,17 @@ class OwnFullProfileView(APIView):
         serializer = FullProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
-    def put(self, request):
-        profile = Profile.objects.get(user=request.user)
-        serializer = FullProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request):
+    #     profile = Profile.objects.get(user=request.user)
+    #     serializer = FullProfileSerializer(profile, data=request.data, context={'request': request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request):
         profile = Profile.objects.get(user=request.user)
-        serializer = FullProfileSerializer(profile, data=request.data, partial=True, context={'request': request})
+        serializer = FullProfileSerializer(instance = profile, data=request.data, partial=True, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
