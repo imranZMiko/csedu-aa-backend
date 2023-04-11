c116c82151143fd6ecffce62ef5a42484035037e
Mohidul Haque Mridul <mridulhaque@Mohiduls-MacBook-Air.local>
2023-04-11 22:32:48 +0600

    fixed the user creation api

--- users/serializers/user.py
+++ users/serializers/user.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
-from users.models import Referral
+from users.models import Referral, Profile
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    referral = serializers.CharField(required=True, write_only=True)
+    referral_code = serializers.CharField(required=True, write_only=True)
+    first_name = serializers.CharField(required=True, write_only=True)
+    last_name = serializers.CharField(required=True, write_only=True)
+    batch_number = serializers.CharField(required=True, write_only=True)
+    sex = serializers.ChoiceField(choices=Profile.SEX_CHOICES, required=True, write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email_address', 'password', 'referral', 'referred_by']
+        fields = ['id', 'username', 'email_address', 'password', 'referral_code', 'referred_by', 'first_name', 'last_name', 'batch_number', 'sex']
         extra_kwargs = {
             'password': {'write_only': True},
             'username': {'validators': []},
@@ -18,20 +22,29 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         referral = validated_data.pop('referred_by', None)
-        referred_email = validated_data.pop('referred_email', None)
+        email_address = validated_data.pop('email_address', None)
         if referral:
             raise serializers.ValidationError('Cannot contain referred_by field')
-        referral_code = validated_data.pop('referral')
+        referral_code = validated_data.pop('referral_code')
         try:
             referral = Referral.objects.get(referral_code = referral_code)
             referral_user = referral.referrer
         except ObjectDoesNotExist:
             raise serializers.ValidationError('Invalid referral')
-        if referred_email != referral.referred_email:
+        if email_address != referral.referred_email:
             raise serializers.ValidationError('The referral should be used with the same email it was created for.')
-        user = User(**validated_data, referred_by=referral_user)
+        
+        first_name = validated_data.pop('first_name', None)
+        last_name = validated_data.pop('last_name', None)
+        batch_number = validated_data.pop('batch_number', None)
+        sex = validated_data.pop('sex', None)
+
+        user = User(**validated_data, referred_by=referral_user, email_address = email_address)
         user.set_password(validated_data['password'])
         user.save()
+        # Create a profile instance for the newly created user
+
+        Profile.objects.create(user=user, first_name=first_name, last_name=last_name, batch_number=batch_number, sex=sex)
         return user
 
     def update(self, instance, validated_data):
--- users/signals.py
+++ users/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Profile
 
-@receiver(post_save, sender=get_user_model())
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=get_user_model())
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
